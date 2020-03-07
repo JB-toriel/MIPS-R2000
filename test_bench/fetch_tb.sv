de5fa6fecd44a7ef2d8d1780bbd0635a3aff69b7
J-B <59450754+JB-toriel@users.noreply.github.com>
2020-03-07 16:04:54 +0100

    Update fetch_tb.sv

--- test_bench/fetch_tb.sv
+++ test_bench/fetch_tb.sv
@@ -1,3 +1,11 @@
+//-----------------------------------------------------
+	// This is the test bench of th fetch stage design 
+	// Design Name : fetch_tb
+	// File Name   : fetch_tb.sv
+	// Function    : 
+	// Authors     : de Sainte Marie Nils - Edde Jean-Baptiste
+//-----------------------------------------------------
+
 /*
 module NOM (LISTE DES PORTS);
 
@@ -17,36 +25,31 @@ endmodule
 
 module fetch_tb; 
   
-     reg clk; 
-     reg br, except;
+	reg clk; 
+	reg br, except;
 
-// for pc register
-  reg [31:0] pc_out;
+	// for pc register
+	reg [31:0] pc_out;
 
-  // for pc mux
-  reg [31:0] sign, fixed;
+	// for pc mux
+	reg [31:0] sign, fixed;
+	
+	parameter CLK_PERIOD = 10;
+	
+	always #(CLK_PERIOD/2.0) clk = ~clk;
+	
+	// Instantiation of design under test
+	IF Instruction_fetch ( clk, sign, fixed, br, except, pc_out );
 
-  parameter CLK_PERIOD = 10;
+	// Test bench starts Here
+	initial
+		begin
+			sign = 0; fixed = 0;
+			br = 0; except = 0;
+			clk = 1;
+			#100
+			$display( "End of simulation time is %d", $time );
+			$finish(1);
+		end
 
-  always #(CLK_PERIOD/2.0) clk = ~clk;
-
- // Instantiate design under test
-  IF Instruction_fetch ( clk, sign, fixed, br, except, pc_out );
-
-  /*task display;
-    #1 $display( "pc = %0h - pc_4 = %0h", pc, pc_4);
-  endtask
-  */
-
-  initial
-        begin
-                sign = 0; fixed = 0;
-                br = 0; except = 0;
-                clk = 1;
-                #100
-				$display( "End of simulation time is %d", $time );
-                $finish(1);
-        end
-
-
-endmodule
+endmodule // End of Module fetch_tb
