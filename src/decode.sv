e31e6f094096b9e6febc4d18eed34825cc2ffa3c
J-B <59450754+JB-toriel@users.noreply.github.com>
2020-03-20 10:35:26 +0100

    decode stage

---
+++ src/decode.sv
@@ -0,0 +1,36 @@
+//-----------------------------------------------------
+	// This is design of the fetch stage of the pipeline
+	// Design Name : IF
+	// File Name   : fetch.sv
+	// Function    : 
+	// Authors     : de Sainte Marie Nils - Edde Jean-Baptiste
+//-----------------------------------------------------
+
+/*
+module NOM (LISTE DES PORTS);
+
+DECLARATION DES PORTS
+DECLARATION DES PARAMETRES
+
+`include "NOM DE FICHIER";
+
+DECLARATIONS DE VARIABLES
+AFFECTATIONS
+INSTANCIATIONS DE SOUS-MODULES
+BLOCS initial ET always
+TACHES ET FONCTIONS
+
+endmodule
+*/
+
+/*
+	Inputs : internally must always be of type net, externally the inputs can be connected to a variable of type reg or net.
+	Outputs : internally can be of type net or reg, externally the outputs must be connected to a variable of type net.
+	Inouts : internally or externally must always be type net, can only be connected to a variable net type.
+*/
+
+
+module ID ( inst_in/*, ...*/);	
+	input [31:0] inst_in;
+	
+	endmodule // End of ID module
