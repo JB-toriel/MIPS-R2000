2598a48b1a42a596e1f70c9d576d08f238ea7e84
Hellmett50 <62420263+Hellmett50@users.noreply.github.com>
2020-04-10 15:49:36 +0200

    Update fetch.sv

--- src/fetch.sv
+++ src/fetch.sv
@@ -46,34 +46,28 @@ module fetch_PC_REG ( clk, old_pc, new_pc );
 endmodule	// End of Module fetch_PC_REG
 
 
-module fetch_MUX( inc_pc, sign, fixed, br, except, new_pc );
+module fetch_MUX( inc_pc, pc_branch, br, except, new_pc );
 
 	// Inputs Declaration
 	input [31:0] inc_pc;	//old_pc "+ 4"
-	input [31:0] sign, fixed;
+	input [31:0] pc_branch;
 	input br, except;
 
 	// Outputs Declaration
 	output reg [31:0] new_pc;
 
-
 	// Code starts Here
 	always @( br or except or inc_pc )
       begin
-		if ( br == 0 && except == 0 )
-			 new_pc = inc_pc + 4;
-
-		if ( br == 0 && except == 1 )
-			 new_pc = except;
-
-		if ( br == 1 && except == 0 )
-			 new_pc = sign;
-
-		if ( br == 1 && except == 1 )
-			 new_pc = 32'hFFFF_FFFF;
+				case ({br,except})
+					0: new_pc = inc_pc + 4;
+					1: new_pc = 32'h4000_0040;
+					2: new_pc = pc_branch;
+					default: new_pc = inc_pc + 4;
+				endcase
 	  end
 
-  	initial new_pc=0;
+		initial new_pc = 0;
 
 endmodule // End of Module fetch_MUX
 
@@ -92,7 +86,7 @@ module fetch_ROM ( clk, pc/*, chip_en, read_en*/, inst );
 	output reg [31:0] inst;
 
 	//------Variable declaration------//
-	reg [31:0] rom_code [0:8];
+	reg [31:0] rom_code [0:10];
 
 
 	// Code starts Here
@@ -115,11 +109,11 @@ module fetch_ROM ( clk, pc/*, chip_en, read_en*/, inst );
 endmodule	// End of Module fetch_ROM
 
 
-module IF( clk, sign, fixed, br, except, pc_out, inst_out);
+module IF( clk, pc_branch, br, except, pc_out, inst_out);
 
 	// Inputs Declaration
 	input clk;
-	input [31:0] sign, fixed;
+	input [31:0] pc_branch;
 	input br, except;
 
 	// Outputs Declaration
@@ -143,21 +137,20 @@ module IF( clk, sign, fixed, br, except, pc_out, inst_out);
 
 	fetch_MUX mux(
 
-		.inc_pc (	pc		), // input	[31:0]
-		.sign   (	sign  	), // input	[31:0]
-		.fixed  (	fixed   ), // input	[31:0]
-		.br  	(	br   	), // input
-		.except (	except  ), // input
-		.new_pc (	pc_4    )  // output	[31:0]
+		.inc_pc 	(	pc				), // input	[31:0]
+		.pc_branch(	pc_branch	), // input	[31:0]
+		.br  			(	br   			), // input
+		.except 	(	except  	), // input
+		.new_pc 	(	pc_4    	)  // output	[31:0]
 
 	);
 
 	fetch_ROM rom(
 
-		.clk	(	clk			 ),
-		.pc		(	pc_4		 ), // input	[31:0]
+		.clk	(	clk					),
+		.pc		(	pc_4				), // input	[31:0]
 		/*.chip_en(	chip_en	), // input
-		.read_en(	read_en	), // input*/
+		.read_en(	read_en		), // input*/
 		.inst	(	old_inst_out )  // output	[31:0]
 
 	);
