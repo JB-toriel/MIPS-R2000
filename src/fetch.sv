1df472485a7be87e17be408486022ffb6df0e4a8
JB-toriel <59450754+JB-toriel@users.noreply.github.com>
2020-04-21 14:41:35 +0200

    Update fetch.sv

--- src/fetch.sv
+++ src/fetch.sv
@@ -40,16 +40,16 @@ module fetch_PC_REG ( clk, hold_pc, old_pc, new_pc );
 	output reg [31:0] old_pc;
 
 	//------Code starts Here------//
-	always_ff @(posedge clk)
+	always_ff @( posedge clk )
 		begin
-			if (hold_pc==0)
+			if ( hold_pc==0 )
 				old_pc <= new_pc;
 		end
 
 endmodule // End of Module fetch_PC_REG
 
 
-module fetch_MUX( inc_pc, pc_branch, br, except, new_pc );
+module fetch_MUX ( inc_pc, pc_branch, br, except, new_pc );
 
 	//Inputs Declaration
 	input [31:0] inc_pc;	//old_pc "+ 4"
@@ -64,14 +64,14 @@ module fetch_MUX( inc_pc, pc_branch, br, except, new_pc );
 	initial new_pc = 0;
 
 	always @( br or except or inc_pc )
-      begin
-				case ({br,except})
+		begin
+				case ( {br,except} )
 					0: new_pc = inc_pc + 4;
 					1: new_pc = 32'h8000_0180;
 					2: new_pc = pc_branch - 4;
 					default: new_pc = inc_pc + 4;
 				endcase
-	  end
+		end
 
 endmodule // End of Module fetch_MUX
 
@@ -87,7 +87,7 @@ module fetch_ROM ( clk, pc/*, chip_en, read_en*/, inst );
 	*/
 
 	//Ouputs Declaration
-	output reg [31:0] inst;
+	output [31:0] inst;
 
 	//Variables declaration
 	reg [31:0] rom_code [0:50];
@@ -101,16 +101,16 @@ module fetch_ROM ( clk, pc/*, chip_en, read_en*/, inst );
   	else
 		assign inst = 32'hFFFF_FFFF;
 	*/
-
+	
 	initial
 		begin
-			$readmemh("memory.list", rom_code);
+			$readmemh( "memory.list", rom_code );
 		end
 
 endmodule // End of Module fetch_ROM
 
 
-module IF( clk, hold_pc, hold_if, pc_branch, br, except, pc_out, inst_out );
+module IF ( clk, hold_pc, hold_if, pc_branch, br, except, pc_out, inst_out );
 
 	//Inputs Declaration
 	input clk, hold_pc, hold_if;
@@ -166,9 +166,8 @@ module IF( clk, hold_pc, hold_if, pc_branch, br, except, pc_out, inst_out );
 					pc_out <= pc_4;
 					inst_out <= old_inst_out;
 				end
-			if (br) begin
+			if ( br ) 
 				inst_out <= 0;
-			end
 		end
 
 endmodule // End of Module IF
