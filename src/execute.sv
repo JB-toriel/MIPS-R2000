4334ac749149f7a39a94943c0154a1f932c9fc7a
JB-toriel <59450754+JB-toriel@users.noreply.github.com>
2020-04-21 14:41:50 +0200

    Update execute.sv

--- src/execute.sv
+++ src/execute.sv
@@ -32,31 +32,31 @@ endmodule
 
 module forwarding_unit ( rs_id, rt_id, rd_ex, reg_write_ex, rd_wb, reg_write_wb, forward_a, forward_b );
 
-  //Inputs declaration
-  input [4:0] rs_id, rt_id, rd_ex, rd_wb;
-  input reg_write_ex, reg_write_wb;
+	//Inputs declaration
+	input [4:0] rs_id, rt_id, rd_ex, rd_wb;
+	input reg_write_ex, reg_write_wb;
 
-  //Outputs declaration
-  output reg [1:0] forward_a, forward_b;
+	//Outputs declaration
+	output [1:0] forward_a, forward_b;
+	
+	//Variables declaration
+	reg [1:0] fw_a, fw_b;
 
-	  reg [1:0] fw_a, fw_b;
 
-
-  //------Code starts Here------//
-  always @(rs_id, rt_id, rd_ex, rd_wb,reg_write_ex, reg_write_wb)
-    begin
-      	fw_a<=0;
-      	fw_b<=0;
-		if ( reg_write_ex && (rd_ex!=0) && (rd_ex==rs_id) )
-			fw_a<=2;
-      	if ( reg_write_wb && (rd_wb!=0) && rd_ex!=rs_id && rd_wb==rs_id )
-			fw_a<=1;
-		if ( reg_write_ex && (rd_ex!=0) && (rd_ex==rt_id) )
-			fw_b<=2;
-      	if ( reg_write_wb && (rd_wb!=0) && rd_ex!=rt_id && rd_wb==rt_id )
-			fw_b<=1;
-
-    end
+	//------Code starts Here------//
+	always @( rs_id, rt_id, rd_ex, rd_wb,reg_write_ex, reg_write_wb )
+		begin
+			fw_a<=0;
+			fw_b<=0;
+			if ( reg_write_ex && (rd_ex!=0) && (rd_ex==rs_id) )
+				fw_a<=2;
+			if ( reg_write_wb && (rd_wb!=0) && rd_ex!=rs_id && rd_wb==rs_id )
+				fw_a<=1;
+			if ( reg_write_ex && (rd_ex!=0) && (rd_ex==rt_id) )
+				fw_b<=2;
+			if ( reg_write_wb && (rd_wb!=0) && rd_ex!=rt_id && rd_wb==rt_id )
+				fw_b<=1;
+		end
 
 	assign forward_a = fw_a;
 	assign forward_b = fw_b;
@@ -71,7 +71,7 @@ module execute_MUX_RTRD ( rt, rd, ex, write_register );
 	input [5:0] ex;
 
 	//Outputs declaration
-	output reg [4:0] write_register;
+	output [4:0] write_register;
 
 
 	//------Code starts Here------//
@@ -103,31 +103,31 @@ module ALU_ctrl_unit ( ALU_op, fnc_code, ALU_ctrl );
   //parameter SLL = 4'b1010;
 
 	//------Code starts Here------//
-	always @(ALU_op, fnc_code)
+	always @( ALU_op, fnc_code )
 		begin
-        	case(ALU_op)
-            0: ALU_ctrl <= ADD;
-            1: ALU_ctrl <= SUB;
-            2: begin
-					  case(fnc_code)
-						  /*0: ALU_ctrl <= SLL; // Shift left logical
-						  2: ALU_ctrl <= SRL; // rigth*/
-						  3: ALU_ctrl <= SRA; // Arithmetic
-              32: ALU_ctrl <= ADD; // ADD
-              6'b100001: ALU_ctrl <= ADD; // ADDU
-						  34: ALU_ctrl <= SUB; // SUB
-						  36: ALU_ctrl <= AND; // AND
-						  37: ALU_ctrl <= OR;  // OR
-						  38: ALU_ctrl <= XOR;
-						  39: ALU_ctrl <= NOR; // NOR
-						  42: ALU_ctrl <= SLT; // Set on less than
-					 5'h18: ALU_ctrl <= MUL;
-					 5'h1a: ALU_ctrl <= DIV;
-					  	default: ALU_ctrl <= 4'b1111;
-					  endcase
-                end
-            3: ALU_ctrl <= AND;
-            default: ALU_ctrl <= 4'b1111;
+        	case( ALU_op )
+				0: ALU_ctrl <= ADD;
+				1: ALU_ctrl <= SUB;
+				2: begin
+						case(fnc_code)
+							/*0: ALU_ctrl <= SLL; // Shift left logical
+							2: ALU_ctrl <= SRL; // rigth*/
+							3: ALU_ctrl <= SRA; // Arithmetic
+							32: ALU_ctrl <= ADD; // ADD
+							6'b100001: ALU_ctrl <= ADD; // ADDU
+							34: ALU_ctrl <= SUB; // SUB
+							36: ALU_ctrl <= AND; // AND
+							37: ALU_ctrl <= OR;  // OR
+							38: ALU_ctrl <= XOR;
+							39: ALU_ctrl <= NOR; // NOR
+							42: ALU_ctrl <= SLT; // Set on less than
+							6'h18: ALU_ctrl <= MUL;
+							6'h1a: ALU_ctrl <= DIV;
+							default: ALU_ctrl <= 4'b1111;
+						endcase
+					end
+				3: ALU_ctrl <= AND;
+				default: ALU_ctrl <= 4'b1111;
 			endcase
 		end
 
@@ -155,27 +155,27 @@ module ALU ( op_1, sign_ext, op_2, ALU_ctrl, zero, over, res );
 	parameter MUL = 4'b0111;
 	parameter DIV = 4'b1000;
 	parameter SRA = 4'b1001;
-  //parameter SLL = 4'b1010;
+	//parameter SLL = 4'b1010;
 	//parameter JR = 4'b1010;
 	//parameter JALR = 4'b1100;
 
 
 	//------Code starts Here------//
 	assign zero = (res==0); // zero flag = 0 if the result is 0
-  assign over = ((sign_ext[5:0] == 32) & (op_1 > 32'hFFFF_FFFF - op_2)) || ((sign_ext[5:0] == 34) & (op_1 < op_2));
+	assign over = ((sign_ext[5:0] == 32) & (op_1 > 32'hFFFF_FFFF - op_2)) || ((sign_ext[5:0] == 34) & (op_1 < op_2));
 
   //$display("%b ? %b & %b" ~(|ALU_ctrl[3:1]), ALU_ctrl[0], (op_1 < op_2));
 
 
-	always @(ALU_ctrl, op_1, op_2)
+	always @( ALU_ctrl, op_1, op_2 )
 		begin
 			case(ALU_ctrl)
-				AND: res <=   op_1 & op_2; 	   	  	// AND
-				OR:  res <=   op_1 | op_2; 	  	  	// OR
-				ADD: res <=   op_1 + op_2; 		  	// ADD
-				SUB: res <=   op_1 - op_2; 		 	 // SUB
+				AND: res <=   op_1 & op_2; 	   	  	
+				OR:  res <=   op_1 | op_2; 	  	  	
+				ADD: res <=   op_1 + op_2; 		  	
+				SUB: res <=   op_1 - op_2; 		 	
 				XOR: res <= 	 op_1 ^ op_2;
-				NOR: res <= ~(op_1 | op_2); 	   	// NOR
+				NOR: res <= ~(op_1 | op_2); 	   	
 				SLT: res <=   op_1 < op_2 ? 1 : 0;  // Set on less than
 				MUL: res <=	 op_1 * op_2;
 				DIV: res <= 	 op_1 / op_2;
@@ -217,12 +217,11 @@ module EX ( clk, data_1, data_2, rs, rt, rd, ex, m_EX, wb_EX, wb_WB, rd_WB, flus
 	wire [3:0] ALU_ctrl;
 	wire [5:0] fnc_code;
 	wire [31:0] op_1, op_2, op_21;
-
-	reg [31:0] old_res;
-	reg [4:0] old_write_register_ex;
-
 	wire [3:0] m_EX_mux;
 	wire [2:0] wb_EX_mux;
+	
+	reg [31:0] old_res;
+	reg [4:0] old_write_register_ex;
 
 
 	//------Modules Instantiation------//
@@ -246,28 +245,29 @@ module EX ( clk, data_1, data_2, rs, rt, rd, ex, m_EX, wb_EX, wb_WB, rd_WB, flus
 		.res 	  (	old_res		)  // output [31:0]
 	);
 
-  forwarding_unit fw_unit ( rs, rt, write_register_ex, wb_MEM[1], rd_WB, wb_WB[1], forward_a, forward_b);
+	forwarding_unit fw_unit ( rs, rt, write_register_ex, wb_MEM[1], rd_WB, wb_WB[1], forward_a, forward_b );
 
 
 	//------Code starts Here------//
 	assign ALU_op 	= ex[4:1];		  // 2 bits to select which operation to do with the ALU
 	assign fnc_code = imm[5:0]; 	  // function code of R-type instructions
 
-	assign op_1 	= (forward_a==0) ? data_1 : (forward_a==1 ? write_data_reg : (forward_a==2) ? res : 2'hx);
-	assign op_21 	= (forward_b==0) ? data_2 : (forward_b==1 ? write_data_reg : (forward_b==2) ? res : 2'hx);
-	assign op_2 	= ex[0] ? imm : op_21; // Mux to chose between "data_2" or the immediate sign extended
+	assign op_1 	 = (forward_a==0) ? data_1 : (forward_a==1 ? write_data_reg : (forward_a==2) ? res : 2'hx);
+	assign op_21 	 = (forward_b==0) ? data_2 : (forward_b==1 ? write_data_reg : (forward_b==2) ? res : 2'hx);
+	assign op_2 	 = ex[0] ? imm : op_21; // Mux to chose between "data_2" or the immediate sign extended
 	assign op_21_mem = op_21;
 
-	assign m_EX_mux = flush_ex ? 0 : m_EX;
+	assign m_EX_mux  = flush_ex ? 0 : m_EX;
 	assign wb_EX_mux = flush_ex ? 0 : wb_EX ;
 
-	always_ff @ ( posedge clk ) begin
-		m_MEM <= m_EX_mux;
-		wb_MEM <= wb_EX_mux;
-		res <= old_res;
-		write_register_ex <= old_write_register_ex;
-		zero <= old_zero;
-		write_data_ex <= op_21_mem;
-	end
+	always_ff @( posedge clk ) 
+		begin
+			m_MEM <= m_EX_mux;
+			wb_MEM <= wb_EX_mux;
+			res <= old_res;
+			write_register_ex <= old_write_register_ex;
+			zero <= old_zero;
+			write_data_ex <= op_21_mem;
+		end
 
 endmodule // End of module EX
